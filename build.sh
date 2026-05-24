c40d07e1aa2744d361cc3292486b560dd450f0f9
root <root@localhost.localdomain>
2026-05-24 22:27:36 +0800

    fix(build): temporarily revert to upstream repos for web assets
    The iskycc/web-dist and iskycc/alist-web forks are not ready:
    - web-dist missing dev branch and dist files
    - alist-web missing releases with dist.tar.gz
    
    Revert to original upstream sources until forks are properly set up:
    - web-dist: alist-org/web-dist (dev branch)
    - alist-web: AlistGo/alist-web (releases)
    
    TODO: Revert this commit once iskycc forks have proper content

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ elif [ "$1" = "beta" ]; then
 else
   git tag -d beta
   version=$(git describe --abbrev=0 --tags)
-  webVersion=$(wget -qO- -t1 -T2 "https://api.github.com/repos/iskycc/alist-web/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+  webVersion=$(wget -qO- -t1 -T2 "https://api.github.com/repos/AlistGo/alist-web/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 fi
 
 echo "backend version: $version"
@@ -28,7 +28,7 @@ ldflags="\
 "
 
 FetchWebDev() {
-  curl -L https://codeload.github.com/iskycc/web-dist/tar.gz/refs/heads/dev -o web-dist-dev.tar.gz
+  curl -L https://codeload.github.com/alist-org/web-dist/tar.gz/refs/heads/dev -o web-dist-dev.tar.gz
   tar -zxvf web-dist-dev.tar.gz
   rm -rf public/dist
   mv -f web-dist-dev/dist public
@@ -36,7 +36,7 @@ FetchWebDev() {
 }
 
 FetchWebRelease() {
-  curl -L https://github.com/iskycc/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
+  curl -L https://github.com/AlistGo/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
   tar -zxvf dist.tar.gz
   rm -rf public/dist
   mv -f dist public
